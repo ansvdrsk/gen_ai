9756e3265402c920aa56ca26291618a884e0b2da
Anastasiia Sviderska <49154158+ansvdrsk@users.noreply.github.com>
2024-12-21 19:48:55 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config:
-    OPENSEARCH_HOST = '149.130.211.131'  
+    OPENSEARCH_HOST = 'XXX.XXX.XXX.XXX'  
     OPENSEARCH_PORT = 9200
-    OPENSEARCH_USER = "anna"
-    OPENSEARCH_PASSWORD = "Qwer0212@"
+    OPENSEARCH_USER = "XXX"
+    OPENSEARCH_PASSWORD = "XXXX"
 
    
     REDIS_HOST = '127.0.0.1'
@@ -16,8 +16,8 @@ class Config:
     OCI_CONFIG_FILE = '~/.oci/config'
     OCI_DEFAULT_PROFILE = 'DEFAULT'
     OCI_CHICAGO_PROFILE = 'CHICAGO'
-    OCI_ENDPOINT = "https://inference.generativeai.us-chicago-1.oci.oraclecloud.com"
-    OCI_COMPARTMENT_ID = "ocid1.compartment.oc1..aaaaaaaafnqwr4bxz4vhjlvfeecxzxh7ztjn7u6szwevj3uwgjthwnt7nebq"
+    OCI_ENDPOINT = "XXX"
+    OCI_COMPARTMENT_ID = "XXX"
     OCI_NAMESPACE = "oraseemeail"
     OCI_BUCKET_NAME = "anastasiia"
 
